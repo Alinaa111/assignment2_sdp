25cd09e20b2d6e2effba4bf1f3fc50c63f6fb01c
alina <alinamoldabaeva1704@gmail.com>
2025-09-19 21:38:13 +0500

    Added Concrete Factories for Win and Mac

---
+++ src/gui/WinFactory.java
@@ -0,0 +1,13 @@
+package gui;
+
+public class WinFactory implements GUIFactory{
+    @Override
+    public Button createButton() {
+        return new WinButton();
+    }
+
+    @Override
+    public Checkbox createCheckbox() {
+        return new WinCheckbox();
+    }
+}
